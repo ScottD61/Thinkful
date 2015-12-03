08b3afaba624ab6455c95d88e3d828fe31a825c6
Scott Davis <ScDavis6@gmail.com>
2015-12-02 16:25:07 -0800

    Update

--- Unit 4/overfitting.py
+++ Unit 4/overfitting.py
@@ -25,22 +25,28 @@ train_df = pd.DataFrame({'X': train_X, 'y': train_y})
 test_df = pd.DataFrame({'X': test_X, 'y': test_y})
 
 #Model Building
-#Numpy objects
 #Linear regression
 poly_1 = smf.ols(formula = 'y ~ 1 + X', data = train_df).fit()
 #Quadratic 
 poly_2 = smf.ols(formula = 'y ~ 1 + X + I(X**2)', data = train_df).fit()
 
+
 #Test
-#Linear model
-ols_predict = poly_1.predict(train_df)
+#Numpy objects
+#Linear model - getting 1k results for some reason 
+ols_predict = poly_1.predict(train_df['X'])
 #Quadratic model
-quad_predict = poly_2.predict(train_df)
+quad_predict = poly_2.predict(train_df['X'])
 
 #Evaluation with training mean-square error (MSE)
 #Linear model
 mean_squared_error(train_df['y'], ols_predict)
-#4.056
 #Quadratic model
 mean_squared_error(train_df['y'], quad_predict)
-#3.79
+
+
+ols_predict.shape
+#(1000,)
+
+
+print(ols_predict)
